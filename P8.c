8078d7980fbb284b2b87c08d16c9b416187088d9
Renata Moreira <38091053+renatamorcsufu@users.noreply.github.com>
2018-07-04 22:34:49 -0300

    Add files via upload

--- P8.c
+++ P8.c
@@ -1,48 +1,106 @@
 //Renata Moreira da Costa
-// 11721EBI008
+//11721EBI008
+
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-void gera_de_numeros(float * vetor, int tamanho) {
-	int i;  
-	for(i = 0; i < tamanho; i++) 
-		vetor[i] = (float) rand()/(float) RAND_MAX + 0.5f;}
-float soma_de_vet(float v[], int numer){
-	 if (numer == 0) return 0;
-   else {
-      float s;
-      s = soma_de_vet (v, numer-1);
-      if (v[numer-1] > 0) s += v[numer-1];
-      return (s);}
-	  }
-float multp(float v[], int numer){
-	float mult = 1.0f;
-	int i;
-	for( i=0; i<numer; i++)
-		mult = mult*v[i];
-	return mult;
-}
 
-int main(){
-	srand(123456);
-	//printf("%d\n", rand()); 
-	int opcao, numer=100;
-	float num[100];
-	gera_de_numeros(num, numer);
-	do{
-	printf("\nDigite: \n1 - Somatorio.\n2 - Produto.\n3 - Sair do prgrama.");
-	printf("\nDigite a opcao: ");
-	scanf("%d", &opcao);	
+struct d
+{
+	float largura;
+	float profundidade;
+	float altura;
+};
+
+struct prod
+{
+	char nome[64];
+	float preco;
+	struct d end;
+};
+void cadastro(struct prod vetor_prod[], int i){
+	printf("\nDigite o nome:");
+	fgets(vetor_prod[i].nome, 64, stdin);
+	printf("\nDigite o preco:");
+	scanf("%f", &vetor_prod[i].preco);
 	getchar();
-	switch(opcao){
-		case 1:
-			printf("\nSomatorio: %f", soma_de_vet(num, numer));
+	printf("\nDigite a largura:");
+	scanf("%f", &vetor_prod[i].end.largura);
+	getchar();
+	printf("\nDigite a profundidade:");
+	scanf("%f", &vetor_prod[i].end.profundidade);
+	getchar();
+	printf("\nDigite a altura:");
+	scanf("%f", &vetor_prod[i].end.altura);
+	getchar();
+}
+void pesquisa(struct prod vetor_prod[], int i){
+	char condicional[64];
+	strcpy(condicional, " ");
+	if(strcmp(condicional, vetor_prod[i].nome) == 0){
+		printf("\nProduto nao cadastrado!");
+	}
+	else{
+		printf("\n %s, R$ %.2f, L: %.2fm x P: %.2fm x A: %.2fm",vetor_prod[i].nome, vetor_prod[i].preco, vetor_prod[i].end.largura, vetor_prod[i].end.profundidade, vetor_prod[i].end.altura);
+	}
+}
+int main()
+{
+	struct prod vetor_prod[2];
+	int opcao, prod, numer_prod = 0;
+	strcpy(vetor_prod[0].nome, " ");
+	strcpy(vetor_prod[1].nome, " ");
+	do{
+	printf("\n\n1 - Cadastro\n2 - Consulta\n3 - Sair\n\nDigite uma opcao: ");
+	scanf("%d", &opcao);
+	getchar();  
+	switch(opcao)
+	{
+		case 1: 
+			printf("\nEscolha o produto 1 ou 2:");
+			scanf("%d", &prod);
+			getchar();
+			switch(prod){
+				case 1:
+					numer_prod= prod -1;
+					cadastro(vetor_prod, numer_prod);
+					printf("\nProduto 1 cadastrado com sucesso");
+					numer_prod = 0;
+					break;
+				case 2:
+					numer_prod = prod -1;
+					cadastro(vetor_prod, numer_prod);
+					printf("\nProduto 2 cadastrado com sucesso");
+					numer_prod = 0;
+					break;
+				default:
+					break;
+			}
 			break;
-		case 2:
-			printf("\nProduto: %f", multp(num,numer));
+		case 2:	
+			printf("\nEscolha o produto 1 ou 2:");
+			scanf("%d", &prod);
+			getchar();
+			switch(prod){
+				case 1:
+					numer_prod = prod -1;
+					pesquisa(vetor_prod, numer_prod);
+					numer_prod = 0;
+					break;
+				case 2:
+					numer_prod = prod -1;
+					pesquisa(vetor_prod, numer_prod);
+					numer_prod = 0;
+					break;
+				default:
+					break;
+			}
 			break;
 		case 3:
 			return 0;
-		default:
+		default: 
 			break;
-	}}while(opcao != 4);
-	return 0;}
+	}	
+	}while(opcao != 4);
+	return 0;
+}
